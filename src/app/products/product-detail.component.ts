fc1c6fc6d3852d3d272d712cd33504c971349f92
Beatriz Parejo Ramos <32536069+BeatrizPR@users.noreply.github.com>
2019-01-18 14:03:55 +0100

    continue the application

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
-import{ ActivatedRoute} from '@angular/router';
+import{ ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -11,7 +11,7 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     let id = +this.route.snapshot.paramMap.get('id');
@@ -30,5 +30,11 @@ export class ProductDetailComponent implements OnInit {
       'imageUrl': 'https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png'
     }
   }
+
+  // return to page product list
+  onBack(): void{
+    this.router.navigate (['/products']);
+  }
+
 
 }
---
+++ src/app/products/product.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { ProductListComponent } from './product-list.component';
+import { ProductDetailComponent } from './product-detail.component';
+import { RouterModule } from '@angular/router';
+import { ProductDetailGuard } from './product-detail.guard';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  declarations: [
+    ProductListComponent,
+    ProductDetailComponent,
+  ],
+  imports: [
+    RouterModule.forChild([
+      {path: 'products', component: ProductListComponent },
+      {path: 'product/:id',
+        canActivate:[ ProductDetailGuard],
+        component:ProductDetailComponent },
+    ]),
+    SharedModule
+  ]
+})
+export class ProductModule { }
